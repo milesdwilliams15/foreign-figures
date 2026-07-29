5ae0ad9e57a7f06c768d44479bd6b3227bd6aec3
milesdwilliams15 <milesdwilliams15@gmail.com>
2026-07-29 14:43:45 -0400

    more fixes

--- _helpers/splot.R
+++ _helpers/splot.R
@@ -5,37 +5,42 @@ source(here::here("my_theme.R"))
 
 # create smart plot function
 splot <- function(data, ...,
-                  type = "scatter",
+                  type = "point",
                   hjust = 0.5, vjust = 0.5, 
+                  pal = "qualitative",
+                  aes = "color",
                   xangle = FALSE,
-                  color = "black", fill = "gray",
-                  shape = 19, linetype = 1,
-                  size = 2, method = "gam", formula = y ~ s(x),
+                  method = "gam", formula = y ~ s(x),
                   title = NULL, subtitle = NULL,
                   xlab = NULL, ylab = NULL) {
+  
+  ## set global default for all future geom_point calls
+  update_geom_defaults("point", list(color = "black"))
+  update_geom_defaults("line", list(color = "black"))
+  update_geom_defaults("col", list(fill = "gray"))
   
   ## base plot with user-supplied aesthetics
   p <- ggplot(data, aes(...))
   
   ## add layers conditionally based on selected type(s)
-  if ("scatter" %in% type) p <- p + geom_point(color = color, shape = shape, size = size)
-  if ("whisker" %in% type) p <- p + geom_pointrange(color = color, shape = shape, size = size, linetype = linetype) + geom_vline(xintercept = 0, lty = 2)
-  if ("vwhisker" %in% type)p <- p + geom_pointrange(color = color, shape = shape, size = size, linetype = linetype) + geom_hline(yintercept = 0, lty = 2)
-  if ("column" %in% type)  p <- p + geom_col(fill = fill) 
-  if ("hist" %in% type)    p <- p + geom_histogram(fill = fill)
-  if ("smooth" %in% type)  p <- p + stat_smooth(color = color, fill = fill, linetype = linetype, method = method, formula = formula)
-  if ("line" %in% type)    p <- p + geom_line(color = color, linetype = linetype, linewidth = 1)
-  if ("text" %in% type)    p <- p + geom_text(fontface = "bold", hjust = hjust, vjust = vjust, color = color)
+  if ("point" %in% type) p <- p + geom_point()
+  if ("whisker" %in% type) p <- p + geom_pointrange() + geom_vline(xintercept = 0, lty = 2)
+  if ("vwhisker" %in% type)p <- p + geom_pointrange() + geom_hline(yintercept = 0, lty = 2)
+  if ("column" %in% type)  p <- p + geom_col() 
+  if ("hist" %in% type)    p <- p + geom_histogram()
+  if ("smooth" %in% type)  p <- p + stat_smooth(method = method, formula = formula)
+  if ("line" %in% type)    p <- p + geom_line(linewidth = 1)
+  if ("text" %in% type)    p <- p + geom_text(fontface = "bold", hjust = hjust, vjust = vjust)
 
   ## return plot with custom theme and labels
   p <- p + 
     my_theme() + 
     labs(title = title, subtitle = subtitle, x = xlab, y = ylab,
-           caption = logo)
+           caption = logo) +
+    ggpal(type = pal, aes = aes)
   
   ## update angle of x-axis?
   if (xangle) p <- p + theme(axis.text.x = element_text(angle = 45, hjust = 1))
   
   suppressWarnings(print(p))
 }
-    
